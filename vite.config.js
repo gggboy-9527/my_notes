edbe196cadc06b2fdb69b0f39081121ab238f213
ggboy <517513848@qq.com>
2025-12-11 13:26:47 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,27 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: mode === 'production'
+      ? '/my_notes/'  // 你的仓库名称，末尾要有斜杠
+      : '/',
+    server: {
+      host: 'localhost',
+      port: 5173,
+      strictPort: true,
+      open: true
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets'
     }
-  },
-  base: mode === 'production'
-            ? '/my_notes/'  // 你的仓库名称，末尾要有斜杠
-            : '/',
-  server: {
-    host: 'localhost',
-    port: 5173,
-    strictPort: true,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
   }
 })
 
